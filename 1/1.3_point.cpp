d65da24041233ccdd779fc14253ec7391d05a29d
kingdelee <137211319@qq.com>
2022-01-07 16:41:31 +0800

    git

--- 1/1.3_point.cpp
+++ 1/1.3_point.cpp
@@ -24,5 +24,48 @@ int main() {
     cout << "*pppi:" << *pppi << endl;
     cout << "*ppppi:" << *ppppi << endl;
 
+    // &* 等价于 *& 等价于 &i 等价于 p
+    cout << "pi:" << pi << endl;
+    cout << "&*pi:" << &*pi << endl;
+    cout << "*&pi:" << *&pi << endl;
+
+    cout << "*&i_1:" << *&i_1 << endl;
+
+
+    // 返回 i；i值 ++
+    // *pi即a；等价于 i++
+    cout << "(*pi)++:" << (*pi)++ << endl;
+
+    // 返回i; pi的地址 ++
+    // 等价于 *(pi++)
+    cout << "*pi++:" << *pi++ << endl;
+    cout << "*(pi++):" << *pi++ << endl;
+
+    //
+    cout << "pi:" << pi << endl;
+//    cout << "*pi:" << *pi << endl;
+
+    // 编译失败
+//    cout << "&*i_1:" << &*i_1 << endl;
+
+    int a, b;
+    int *p1, *p2;
+    a = 100;
+    b = 200;
+    p1 = &a;
+    p2 = &b;
+    int *p3 = &a;
+
+    cout << "p1:" << p1 << endl;
+    cout << "p2:" << p2 << endl;
+    cout << "&p2:" << &p1 << endl;
+    cout << "&p2:" << &p2 << endl;
+    cout << "*p2++:" << *p2++ << endl;
+    cout << "p2:" << p2 << endl;
+    cout << "*p2:" << *p2 << endl;
+
+
+
+
     cout << endl;
 }
